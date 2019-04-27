8a01a4503372a9373a79d12b30fad1a28095d046
Peter-Li1233 <minxiangli08@gmail.com>
2019-04-27 07:32:01 -0400

    Adding Base code

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log("Hello, Universe!")
+let name = "Peter Li"
+console.log(name)
+class Goat {
+eat(footType) {
+    console.log(`I love eating ${footType}`);
+      }
+    }
+    let peter = new Goat()
+    peter.eat("tin cans")
+    
+console.log(peter.eat('pizzas'))
