05a899e5dc70e34c6f5f457ca529fba888327305
Peter-Li1233 <minxiangli08@gmail.com>
2019-04-27 08:10:46 -0400

    Added files and changes required for deployment to Heroku

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ var app = express();
 
 app.use(helmet());
 
+//set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://peter:peter123@cluster0-x1srf.mongodb.net/peter_library?retryWrites=true';
+var dev_db_url = 'mongodb+srv://peter:peter123@cluster0-x1srf.mongodb.net/peter_library?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
